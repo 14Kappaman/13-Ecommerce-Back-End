337893683c4463946e98294a21b3830a07a50d1a
Aubrey Crook <aubreycrook@hotmail.com>
2022-02-05 16:37:38 -0500

    Created route for id and category_name

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,15 +6,32 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll()
+  .then(data => {
+    res.json(data);
+
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findAll({
+    where: {
+      id: req.params["id"]
+    }
+  })
+  .then(data => {
+    res.json(data)
+  })
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+  Category.create({
+    category_name: req.body["category_name"]
+  })
+  .then(() => {
+    res.sendStatus(200)
+  })
 });
 
 router.put('/:id', (req, res) => {
