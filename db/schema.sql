91302b53c2b015c0e55e05cf8c53037e0f38d586
Aubrey Crook <aubreycrook@hotmail.com>
2022-02-05 15:19:13 -0500

    Built schema and executed create table statements

--- db/schema.sql
+++ db/schema.sql
@@ -3,4 +3,25 @@ DROP DATABASE IF EXISTS ecommerce_db;
 
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
+use ecommerce_db;
 
+Create Table Category (
+    id int not null primary key auto_increment,
+    Category_Name text not null
+);
+Create Table Product (
+    id int not null primary key auto_increment,
+    Product_Name text not null, 
+    price decimal not null,
+    stock int not null default(10),
+    category_id int references Category(id)
+);
+Create Table Tag(
+    id int not null primary key auto_increment,
+    tag_name text 
+);
+Create Table ProductTag(
+    id int not null primary key auto_increment,
+    product_id int references Product (id),
+    tag_id int references Tag (id)
+);
